42ad25dea340d40b6737fae4da30fafc14c66ca8
PineappleOrca <kevinksingh05@gmail.com>
2026-05-27 18:10:32 +0100

    updated pro yasuos in teh get account list

--- my_secrets.py
+++ my_secrets.py
@@ -17,20 +17,21 @@ def get_my_accounts():
     return account_list 
 
 def get_other_accounts():
-    print("Currently Not a supported/Active function, since 2023 hasnt been updated!")
-    account_list = ['']
+    account_list = [os.getenv("BROVY_PUUID"), os.getenv("TWP_1_PUUID"), os.getenv("TWP_2_PUUID")]
     return account_list 
 
 def get_korea_accounts():
-    print("Currently Not a supported/Active function, since 2023 hasnt been updated!")
-    account_list = ['']
+    account_list = [os.getenv("PZZANG_1_PUUID"), os.getenv("PZZANG_2_PUUID"), os.getenv("PZZANG_3_PUUID")]
     return account_list
 
 def get_account_map() -> dict:
     return {
         os.getenv("MAIN_ACC"): os.getenv("MAIN_NAME"),
         os.getenv("SMURF_1"): os.getenv("SMURF_1_NAME"),
-        os.getenv("SMURF_2"): os.getenv("SMURF_2_NAME")
+        os.getenv("SMURF_2"): os.getenv("SMURF_2_NAME"),
+        os.getenv("BROVY_PUUID"): os.getenv("BROVY_IGN"),
+        os.getenv("TWP_1_PUUID"): os.getenv("TWP_1_IGN"),
+        os.getenv("TWP_2_PUUID"): os.getenv("TWP_2_IGN")
     }
 
 def get_ign(account_id: str) -> str:
